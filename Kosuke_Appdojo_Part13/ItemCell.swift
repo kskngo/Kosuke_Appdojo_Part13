87c2d493e14cf7d429da7bca318160a57533b76a
akio0911 <akio0911@gmail.com>
2021-03-14 11:25:01 +0900

    feedback

---
+++ Kosuke_Appdojo_Part13/ItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCell.swift
+//  Kosuke_Appdojo_Part13
+//
+//  Created by akio0911 on 2021/03/14.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var checkMarkImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+}
--- Kosuke_Appdojo_Part13/TableViewController.swift
+++ Kosuke_Appdojo_Part13/TableViewController.swift
@@ -17,32 +17,25 @@ class TableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return fruitsItems.count
+        fruitsItems.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath)
-
-        if fruitsItems[indexPath.row].isChecked {
-            let checkMarkImage = cell.viewWithTag(1) as? UIImageView
-            checkMarkImage!.image = UIImage(named: "check")
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath) as? ItemCell else {
+            return UITableViewCell()
         }
 
-        let label = cell.viewWithTag(2) as? UILabel
-        label!.text = fruitsItems[indexPath.row].name
+        let item = fruitsItems[indexPath.row]
+
+        cell.checkMarkImageView.image = item.isChecked ? UIImage(named: "check") : nil
+        cell.nameLabel.text = item.name
 
         return cell
     }
+}
 
-    struct FruitsItem {
-        let name: String
-        let isChecked: Bool
-    }
+struct FruitsItem {
+    let name: String
+    let isChecked: Bool
 }
